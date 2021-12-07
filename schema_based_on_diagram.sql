fccf3f99bd9a061e9cf8e90552faf49e619e2b02
Luis <lfmnovaes@gmail.com>
2021-12-07 16:48:16 -0300

    Add table treatments and treatments_histories to the .sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,12 +1,25 @@
-CREATE TABLE patients(
-    id SERIAL PRIMARY KEY ,
-    name VARCHAR(50),
-    date_of_birth DATE,
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY ,
+  name VARCHAR(50),
+  date_of_birth DATE
 );
 
-CREATE TABLE medical_histories(
-    id SERIAL PRIMARY KEY,
-    patient_id INT,
-    admitted_at TIMESTAMP,
-    CONSTRAINT patient_id_fk FOREIGN KEY (patient_id) REFERENCES patients(id)
+CREATE TABLE medical_histories (
+  id SERIAL PRIMARY KEY,
+  patient_id INT,
+  admitted_at TIMESTAMP,
+  FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(100),
+  name VARCHAR(100)
+);
+
+CREATE TABLE treatments_histories (
+  medical_history_id INT,
+  treatment_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
